b6b180142f5d2dbf1b905ce3f109527bd05785e9
Saaransh Sharma <saaranshsharma28@gmail.com>
2024-07-17 14:46:38 -0400

    adding analytics

--- centometer-client/app/layout.tsx
+++ centometer-client/app/layout.tsx
@@ -1,19 +1,20 @@
 import type { Metadata } from "next";
-import { Manrope } from 'next/font/google'
-import { cn } from "@/lib/utils"
+import { Manrope } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { Analytics } from "@vercel/analytics/react";
 import "./globals.css";
 
 const fontHeading = Manrope({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-heading',
-})
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-heading",
+});
 
 const fontBody = Manrope({
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-body',
-})
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-body",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -27,11 +28,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cn(
-          'antialiased',
-          fontHeading.variable,
-          fontBody.variable
-        )}>{children}</body>
+      <body
+        className={cn("antialiased", fontHeading.variable, fontBody.variable)}
+      >
+        {children}
+        <Analytics/>
+      </body>
     </html>
   );
 }
