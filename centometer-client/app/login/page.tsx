3efdfdb45ee1ad31d513e4beec05774739d76879
Saaransh Sharma <saaranshsharma28@gmail.com>
2024-08-01 22:09:34 -0400

    setting up google sso

--- centometer-client/app/login/page.tsx
+++ centometer-client/app/login/page.tsx
@@ -3,6 +3,7 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import Image from "next/image";
 import {
   Card,
   CardContent,
@@ -15,26 +16,41 @@ import Link from "next/link";
 import { login } from "@/app/actions/account";
 import { ArrowLeft } from "lucide-react";
 import { useState } from "react";
+import { Separator } from "@/components/ui/separator"
+import { createClient } from '@/utils/supabase/client'
 
 export default function LoginPage() {
   const [error, setError] = useState("");
-  
+  const supabase = createClient();
+
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
 
     const formData = new FormData(e.currentTarget);
-    
+
     const response = await login(formData);
 
     if (response && response.error) {
       setError(response.error.message)
-    } 
+    }
 
   }
 
 
+  const LoginWithGoogle = async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo: `http://localhost:3000/auth/callback`,
+      },
+    })
+  };
+
+
+
   return (
-    <form className="flex items-center justify-center h-screen w-full" onSubmit={handleSubmit}>
+    <div className="flex items-center justify-center h-screen w-full">
+
       <Card className="w-full max-w-md mx-auto">
         <CardHeader>
           <CardTitle className="text-2xl flex items-center gap-2"><Link href={"/"}><ArrowLeft /></Link>Log In</CardTitle>
@@ -43,27 +59,50 @@ export default function LoginPage() {
           </CardDescription>
         </CardHeader>
         <CardContent className="space-y-4">
-          <div className="space-y-2">
-            <Label htmlFor="email">Email</Label>
-            <Input
-              id="email"
-              name="email"
-              type="email"
-              placeholder="someone@example.com"
-              required
-            />
-          </div>
-          <div className="space-y-2">
-            <Label htmlFor="password">Password</Label>
-            <Input id="password" name="password" type="password" placeholder="your password" required />
-            {error && <div className="text-sm text-destructive font-bold">{error}</div>}
-          </div>
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+
+            <div className="space-y-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                name="email"
+                type="email"
+                placeholder="someone@example.com"
+                required
+              />
+            </div>
+            <div className="space-y-2">
+              <Label htmlFor="password">Password</Label>
+              <Input id="password" name="password" type="password" placeholder="your password" required />
+              {error && <div className="text-sm text-destructive font-bold">{error}</div>}
+            </div>
+            <Button className="w-full " type="submit">
+                Log In
+              </Button>
+          </form>
         </CardContent>
         <CardFooter className="flex flex-col gap-4">
-          <Button className="w-full" type="submit">Log In</Button>
-          <p className="self-start">Don&apos;t have an account? <Link href={"/signup"} className="font-bold text-primary ">Sign up here.</Link></p>
+          <div className="flex items-center w-full justify-center overflow-hidden">
+            <Separator className="" />
+            <p className="mx-5">OR</p>
+            <Separator className="" />
+          </div>
+
+
+          <Button className="w-full" variant={"outline"} onClick={LoginWithGoogle}>
+          <Image width={24} height={24} className="mr-2" src="google.svg" loading="lazy" alt="google logo"/>
+            <span>Log in with Google</span></Button>
+          <p className="self-start">
+            Don&#39;t have an account?{" "}
+            <Link href={"/signup"} className="font-bold text-primary ">
+              Sign up here.
+            </Link>
+          </p>
+
         </CardFooter>
       </Card>
-    </form>
+
+    </div>
+
   );
 }
--- centometer-client/app/signup/page.tsx
+++ centometer-client/app/signup/page.tsx
@@ -3,6 +3,8 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { Separator } from "@/components/ui/separator"
+import Image from "next/image";
 import {
   Card,
   CardContent,
@@ -15,27 +17,37 @@ import Link from "next/link";
 import { ArrowLeft } from "lucide-react";
 import { signup } from "../actions/account";
 import { useState } from "react";
-
+import { createClient } from '@/utils/supabase/client'
 
 export default function SignupPage() {
   const [error, setError] = useState("");
+  const supabase = createClient();
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault()
+    e.preventDefault();
 
     const formData = new FormData(e.currentTarget);
-    
+
     const response = await signup(formData);
 
     if (response && response.error) {
-      setError(response.error.message)
-    } 
+      setError(response.error.message);
+    }
+  };
 
-  }
+  const LoginWithGoogle = async () => {
+    await supabase.auth.signInWithOAuth({
+      provider: "google",
+      options: {
+        redirectTo: `http://localhost:3000/auth/callback`,
+      },
+    })
+  };
+
 
   return (
     <>
-      <form className="flex items-center justify-center h-screen w-full" onSubmit={handleSubmit}>
+      <div className="flex items-center justify-center h-screen w-full">
         <Card className="w-full max-w-md mx-auto">
           <CardHeader>
             <CardTitle className="text-2xl flex items-center gap-2">
@@ -49,32 +61,49 @@ export default function SignupPage() {
             </CardDescription>
           </CardHeader>
           <CardContent className="space-y-4">
-            <div className="space-y-2">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                id="email"
-                name="email"
-                type="email"
-                placeholder="someone@example.com"
-                required
-              />
-            </div>
-            <div className="space-y-2">
-              <Label htmlFor="password">Password</Label>
-              <Input
-                id="password"
-                name="password"
-                type="password"
-                placeholder="enter a password"
-                required
-              />
-              {error && <div className="text-sm text-destructive font-bold">{error}</div>}
-            </div>
+            <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+              <div className="space-y-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  name="email"
+                  type="email"
+                  placeholder="someone@example.com"
+                  required
+                />
+              </div>
+              <div className="space-y-2">
+                <Label htmlFor="password">Password</Label>
+                <Input
+                  id="password"
+                  name="password"
+                  type="password"
+                  placeholder="enter a password"
+                  required
+                />
+                {error && (
+                  <div className="text-sm text-destructive font-bold">
+                    {error}
+                  </div>
+                )}
+              </div>
+
+              <Button className="w-full " type="submit">
+                Sign Up
+              </Button>
+            </form>
           </CardContent>
           <CardFooter className="flex flex-col gap-4">
-            <Button className="w-full" type="submit">
-              Sign Up
-            </Button>
+            
+
+            <div className="flex items-center w-full overflow-hidden justify-center">
+            <Separator className="" />
+            <p className="mx-5">OR</p>
+            <Separator className="" />
+            </div>
+
+
+          <Button variant={"outline"} className="w-full" onClick={LoginWithGoogle}><Image width={24} height={24} className="mr-2" src="https://www.svgrepo.com/show/475656/google-color.svg" loading="lazy" alt="google logo"/><span>Sign up with Google</span></Button>
             <p className="self-start">
               Already have an account?{" "}
               <Link href={"/login"} className="font-bold text-primary ">
@@ -82,8 +111,10 @@ export default function SignupPage() {
               </Link>
             </p>
           </CardFooter>
+
+
         </Card>
-      </form>
+      </div>
     </>
   );
 }
