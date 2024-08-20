70698c55e977f511d801eef7244165ebc4fa8051
Saaransh Sharma <saaranshsharma28@gmail.com>
2024-08-19 21:32:22 -0400

    setting up stock search

--- stock-search/index.ts
+++ stock-search/index.ts
@@ -1,9 +1,8 @@
 import { APIGatewayProxyEvent } from "aws-lambda";
+import yahooFinance from "yahoo-finance2";
 
 export const handler = async (event: APIGatewayProxyEvent) => {
-
-  const query = event.queryStringParameters?.query
-
+  const query = event.queryStringParameters?.query;
 
   const response = {
     statusCode: 200,
@@ -11,4 +10,19 @@ export const handler = async (event: APIGatewayProxyEvent) => {
   };
   return response;
 };
- 
+
+const searchStock = async (searchItem: string | undefined) => {
+
+  if (searchItem === undefined) {
+    return
+  }
+
+
+  const results = await yahooFinance.search(searchItem)
+
+  console.log(results)
+
+
+};
+
+searchStock("APPL")
