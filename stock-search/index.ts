6265f4a135cb5a9536b07a1b9d11797e0cde17d8
Saaransh Sharma <saaranshsharma28@gmail.com>
2024-08-20 23:01:28 -0400

    developed searchStock function

--- stock-search/index.ts
+++ stock-search/index.ts
@@ -12,12 +12,43 @@ export const handler = async (event: APIGatewayProxyEvent) => {
 };
 
 export const searchStock = async (searchItem: string | undefined) => {
+  try {
+    const response = await yahooFinance.search(searchItem!, {
+      lang: "en-US",
+      newsCount: 0,
+    });
+    const { quotes } = response;
 
-    try {
-      await yahooFinance.search(searchItem!);
-    } catch (e) {
-      throw Error("could not find stock")
-    }
+    const filteredQuotes = quotes.filter((quote) =>
+      quote.hasOwnProperty("exchDisp")
+    );
 
-}
-//
+    const symbolList = filteredQuotes.map((quote) => quote.symbol);
+
+    const prices = await yahooFinance.quote(symbolList, {
+      fields: ["regularMarketPrice", "regularMarketChangePercent"],
+    });
+
+    const quotesWithPrice = filteredQuotes.map((quote) => {
+      const symbol = quote.symbol;
+
+      const quotePriceObject = prices.find(
+        (priceItem) => priceItem.symbol === symbol
+      );
+
+      const regularMarketPrice = quotePriceObject?.regularMarketPrice || 0;
+      const regularMarketChangePercent =
+        quotePriceObject?.regularMarketChangePercent || 0;
+
+      return {
+        ...quote,
+        regularMarketPrice,
+        regularMarketChangePercent,
+      };
+    });
+
+    return quotesWithPrice;
+  } catch (e) {
+    throw Error("could not find stock");
+  }
+};
