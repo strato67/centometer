bdc0e46ef8932807dd00551642ff52bdc195e8af
Saaransh Sharma <saaranshsharma28@gmail.com>
2024-09-07 23:41:28 -0400

    creating stock page, finished search page

---
+++ centometer-client/app/dashboard/search/search.ts
@@ -0,0 +1,21 @@
+"use server"
+
+export type StockResult = {
+  Index: string,
+  Symbol: string,
+  Description: string
+}
+
+export const getSearchResults = async (searchQuery: string) => {
+    try {
+      const url = process.env.NEXT_PUBLIC_LAMBDA_SEARCH_URL
+      const response = await fetch(`${url?.concat(searchQuery)}`, {cache: "force-cache"})
+      if (!response.ok) {
+        return []
+      }
+      const json = await response.json()
+      return json.message
+    } catch (error) {
+      return []
+    }
+  }
