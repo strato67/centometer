ece7982f69ea5f5185ed187fa86c51592e93834b
Saaransh Sharma <saaranshsharma28@gmail.com>
2024-09-10 21:16:53 -0400

    setting up overview card

--- centometer-client/components/stock/overview-card.tsx
+++ centometer-client/components/stock/overview-card.tsx
@@ -1,26 +1,51 @@
 import {
-    Card,
-    CardContent,
-    CardHeader,
-    CardTitle,
-    
-  } from "@/components/ui/card";
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
 
-export default function OverviewCard(){
-    return(<>
-    
+} from "@/components/ui/card";
+
+export default function OverviewCard() {
+
+  return (<>
+
     <Card className="w-full rounded-b-3xl rounded-t-none p-4 pb-12">
-        <CardHeader className="flex flex-row w-full justify-between">
-          <div className="space-y-1.5">
-            <CardTitle>Overview</CardTitle>
+      <CardHeader className="flex flex-row w-full justify-between">
+        <div className="space-y-1.5">
+          <CardTitle>Overview</CardTitle>
 
-          </div>
+        </div>
 
-        </CardHeader>
-        <CardContent>
+      </CardHeader>
+      <OverviewTable />
+    </Card>
 
-        </CardContent>
-      </Card>
-    
-    </>)
+  </>)
+}
+
+function OverviewTable({ }) {
+
+  const data = { "Previous Close": 242, "Test": 222 }
+
+
+
+  return (<>
+    <CardContent>
+      <div className="grid  grid-cols-2 md:grid-cols-4 gap-x-12 gap-y-2">
+        {
+          Object.entries(data).map(([key, value], index) => {
+            return (
+              <div className="flex justify-between border-b border-b-card-foreground items-center" key={index}>
+                <p>{key}</p>
+                <p className="text-lg font-bold">{value}</p>
+              </div>
+
+            )
+          })
+        }
+      </div>
+    </CardContent>
+
+  </>)
 }
