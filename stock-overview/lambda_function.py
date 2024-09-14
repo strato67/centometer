b9f6676e80514a2d39eacdd46ff9596f7db6df3d
Saaransh Sharma <saaranshsharma28@gmail.com>
2024-09-14 01:11:43 -0400

    test response

--- stock-overview/lambda_function.py
+++ stock-overview/lambda_function.py
@@ -3,14 +3,16 @@ import yfinance as yf
 
 def lambda_handler(event, context):
 
-    msft = yf.Ticker("MSFT")
-
-    print(msft.info)
+    test = company_overview("MSFT")
 
 
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda :-(')
+        'body': json.dumps(test)
     }
 
 
+def company_overview(symbol: str):
+
+    company = yf.Ticker(symbol)
+    return company.info
