9cb6807e9fbe6402504e0188e63eab1dc72381ea
Saaransh Sharma <saaranshsharma28@gmail.com>
2024-09-15 22:05:04 -0400

    company overview lambda

--- stock-overview/lambda_function.py
+++ stock-overview/lambda_function.py
@@ -3,16 +3,45 @@ import yfinance as yf
 
 def lambda_handler(event, context):
 
-    test = company_overview("MSFT")
+    try:
+        if (event['queryStringParameters']) and (event['queryStringParameters']['query']) and (
+            event['queryStringParameters']['query'] is not None):
+                
+            company_symbol = event['queryStringParameters']['query']
+            overview = company_overview(company_symbol)
 
-
-    return {
-        'statusCode': 200,
-        'body': json.dumps(test)
-    }
-
+            return {
+                'statusCode': 200,
+                'headers': {
+                    'Access-Control-Allow-Origin': "*"
+                },     
+                'body': json.dumps({
+                    'message': overview
+                })
+            }
+        else:
+            raise("No search parameters provided")
+    except:
+        
+        return {
+            'statusCode': 500,
+            'headers': {
+                'Access-Control-Allow-Origin': "*"
+            },     
+            'body': json.dumps({
+                'message' : "An error occurred"
+            })
+        }
 
 def company_overview(symbol: str):
 
+    if len(symbol) == 0 or not symbol:
+        raise("No search parameter provided")
+        
     company = yf.Ticker(symbol)
-    return company.info
+    company_info = company.info
+
+    if len(company_info) == 1 and company_info['trailingPegRatio'] == None:
+        raise("No companies found")
+
+    return company_info
