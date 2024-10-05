8c500dd5dd7031ffbd77aa0777481a20cd54309b
Saaransh Sharma <saaranshsharma28@gmail.com>
2024-10-05 19:15:44 -0400

    creating env url

--- centometer-client/components/widgets/symbol-overview.tsx
+++ centometer-client/components/widgets/symbol-overview.tsx
@@ -20,7 +20,7 @@ function SymbolOverview({ticker}:{ticker:string}) {
   "colorTheme": "dark",
   "isTransparent": false,
   "autosize": false,
-  "largeChartUrl": "http://${window.location.host}/dashboard/stock/"
+  "largeChartUrl": "${process.env.NEXT_PUBLIC_SITE_URL}/dashboard/stock/"
 }`;
     if (container.current) {
       container.current.appendChild(script);
--- centometer-client/utils/hooks/sso.ts
+++ centometer-client/utils/hooks/sso.ts
@@ -1,21 +1,16 @@
 import { createClient } from "@/utils/supabase/client";
-import { useEffect, useState } from "react";
 
 export default function useGoogleSSO() {
-  const [domain, setDomain] = useState("");
   const supabase = createClient();
-
-  useEffect(() => {
-    setDomain(window.location.host);
-  }, []);
 
   const LoginWithGoogle = async () => {
     await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: `http://${domain}/auth/callback`,
+        redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/auth/callback`,
       },
     });
+
   };
 
   return { LoginWithGoogle };
