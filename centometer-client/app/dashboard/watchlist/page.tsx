de3684d878203a87b72b204c6ba56a6a55ddf255
Saaransh Sharma <saaranshsharma28@gmail.com>
2024-10-08 15:20:37 -0400

    updates to css styles, prep le watchlist

--- centometer-client/app/dashboard/watchlist/page.tsx
+++ centometer-client/app/dashboard/watchlist/page.tsx
@@ -1,3 +1,10 @@
-export default function Page(){
-    return(<>Watchlist</>)
+export default function Page() {
+  return (
+    <>
+      <div className="my-2 md:my-6 w-full">
+        <h1 className="text-4xl font-bold">Watchlist</h1>
+        
+      </div>
+    </>
+  );
 }
