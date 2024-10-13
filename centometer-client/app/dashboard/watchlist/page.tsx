f07f091855fa158dc2e644c4265843332caa40f4
Saaransh Sharma <saaranshsharma28@gmail.com>
2024-10-12 20:09:17 -0400

    adding search for watchlist

--- centometer-client/app/dashboard/watchlist/page.tsx
+++ centometer-client/app/dashboard/watchlist/page.tsx
@@ -3,12 +3,21 @@ import { StockResult, columns } from "@/components/watchlist/columns";
 
 async function getData(): Promise<StockResult[]> {
   // Fetch data from your API here.
+  
   return [
     {
       id: "dadadw22",
       symbol: "AAPL",
       name: "Apple Inc.",
       price: 225,
+      rating: "buy",
+      index: "NASDAQ",
+    },
+    {
+      id: "dada3w22",
+      symbol: "AMD",
+      name: "Advanced Micro Devices",
+      price: 165,
       rating: "buy",
       index: "NASDAQ",
     },
--- centometer-client/components/watchlist/watchlist-table.tsx
+++ centometer-client/components/watchlist/watchlist-table.tsx
@@ -1,11 +1,13 @@
-"use client"
+"use client";
 
 import {
   ColumnDef,
+  ColumnFiltersState,
   flexRender,
   getCoreRowModel,
+  getFilteredRowModel,
   useReactTable,
-} from "@tanstack/react-table"
+} from "@tanstack/react-table";
 
 import {
   Table,
@@ -14,80 +16,153 @@ import {
   TableHead,
   TableHeader,
   TableRow,
-} from "@/components/ui/table"
-import { useRouter } from "next/navigation"
-import { StockResult } from "./columns"
+} from "@/components/ui/table";
+import { useRouter } from "next/navigation";
+import { StockResult } from "./columns";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import { ChevronDown } from "lucide-react";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import React, { useState } from "react";
 
 interface DataTableProps<TData, TValue> {
-  columns: ColumnDef<TData, TValue>[]
-  data: TData[]
+  columns: ColumnDef<TData, TValue>[];
+  data: TData[];
 }
 
 export function WatchlistTable<TData, TValue>({
   columns,
   data,
 }: DataTableProps<TData, TValue>) {
+
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>(
+    []
+  )
+
   const table = useReactTable({
     data,
     columns,
     getCoreRowModel: getCoreRowModel(),
-  })
+    onColumnFiltersChange: setColumnFilters,
+    getFilteredRowModel: getFilteredRowModel(),
+    state:{
+      columnFilters
+    }
+  });
+
+
 
   const router = useRouter();
 
   return (
-    <div className="rounded-md border">
-      <Table>
-        <TableHeader>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <TableRow key={headerGroup.id}>
-              {headerGroup.headers.map((header) => {
+    <div className="w-full">
+      <div className="flex items-center py-4">
+        <Input
+          placeholder="Search by symbol..."
+          value={(table.getColumn("symbol")?.getFilterValue() as string) ?? ""}
+          onChange={(event) =>
+            table.getColumn("symbol")?.setFilterValue(event.target.value)
+          }
+          className="max-w-sm"
+        />
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="outline" className="ml-auto">
+              Columns <ChevronDown className="ml-2 h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            {table
+              .getAllColumns()
+              .filter((column) => column.getCanHide())
+              .map((column) => {
                 return (
-                  <TableHead key={header.id}>
-                    {header.isPlaceholder
-                      ? null
-                      : flexRender(
+
+                  <React.Fragment key={column.id}>
+                    {column.id !== "symbol" && column.id !== "name" && <DropdownMenuCheckboxItem
+                      
+                      className="capitalize"
+                      checked={column.getIsVisible()}
+                      onCheckedChange={(value) =>
+                        column.toggleVisibility(!!value)
+                      }
+                    >
+                      {column.id}
+                    </DropdownMenuCheckboxItem>}
+
+                    </React.Fragment>
+
+                );
+              })}
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+
+      <div className="rounded-md border">
+        <Table>
+          <TableHeader>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <TableRow key={headerGroup.id}>
+                {headerGroup.headers.map((header) => {
+                  return (
+                    <TableHead key={header.id}>
+                      {header.isPlaceholder
+                        ? null
+                        : flexRender(
                           header.column.columnDef.header,
                           header.getContext()
                         )}
-                  </TableHead>
-                )
-              })}
-            </TableRow>
-          ))}
-        </TableHeader>
-        <TableBody>
-          {table.getRowModel().rows?.length ? (
-            table.getRowModel().rows.map((row) => (
-              <TableRow
-                key={row.id}
-                data-state={row.getIsSelected() && "selected"}
-                onClick={() => {
-                    const stock = row.original as StockResult
+                    </TableHead>
+                  );
+                })}
+              </TableRow>
+            ))}
+          </TableHeader>
+          <TableBody>
+            {table.getRowModel().rows?.length ? (
+              table.getRowModel().rows.map((row) => (
+                <TableRow
+                  key={row.id}
+                  data-state={row.getIsSelected() && "selected"}
+                  className="select-none"
+                  onClick={() => {
+                    const stock = row.original as StockResult;
                     router.push(
                       stock.index === "IDX" || stock.index === "NYSE American"
                         ? `/dashboard/stock/?tvwidgetsymbol=${stock.index}`
                         : `/dashboard/stock/?tvwidgetsymbol=${stock.index}%3A${stock.symbol}`
-                    )
-                }
-                  }
-              >
-                {row.getVisibleCells().map((cell) => (
-                  <TableCell key={cell.id}>
-                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                  </TableCell>
-                ))}
+                    );
+                  }}
+                >
+                  {row.getVisibleCells().map((cell) => (
+                    <TableCell key={cell.id}>
+                      {flexRender(
+                        cell.column.columnDef.cell,
+                        cell.getContext()
+                      )}
+                    </TableCell>
+                  ))}
+                </TableRow>
+              ))
+            ) : (
+              <TableRow>
+                <TableCell
+                  colSpan={columns.length}
+                  className="h-24 text-center"
+                >
+                  No results.
+                </TableCell>
               </TableRow>
-            ))
-          ) : (
-            <TableRow>
-              <TableCell colSpan={columns.length} className="h-24 text-center">
-                No results.
-              </TableCell>
-            </TableRow>
-          )}
-        </TableBody>
-      </Table>
+            )}
+          </TableBody>
+        </Table>
+      </div>
     </div>
-  )
+  );
 }
+
