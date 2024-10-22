2cfc481e82f375ff9ad2b5d72db6eb1a7055a61f
Saaransh Sharma <saaranshsharma28@gmail.com>
2024-10-22 12:56:09 -0400

    initializing supabase

--- watchlist-service/lambda_function.py
+++ watchlist-service/lambda_function.py
@@ -1,13 +1,41 @@
 import json
+import os
+from supabase import create_client, Client
+from dotenv import load_dotenv
+load_dotenv()
+
+url: str = os.environ.get("SUPABASE_URL")
+key: str = os.environ.get("SUPABASE_ANON_KEY")
 
 def lambda_handler(event, context):
 
     user_id = event['queryStringParameters']['id']
+    watchlist = get_watchlist(user_id)
 
     return {
         'statusCode': 200,
         'headers': {
             'Access-Control-Allow-Origin': "*"
         },  
-        'body': json.dumps(user_id)
+        'body': json.dumps({'watchlist': watchlist})
     }
+
+def get_watchlist(id):
+    supabase: Client = create_client(url, key)
+    try:
+        response = supabase.table("watchlist").select("symbol, index").eq("user_id", id).execute()
+        return response.data
+    except:
+        return []
+
+
+
+
+
+
+
+
+
+
+
+
