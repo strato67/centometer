36da630040e8a1184e0508016f3df3cad8e8a59d
Saaransh Sharma <saaranshsharma28@gmail.com>
2024-10-22 23:52:36 -0400

    update to build layer script

--- build_layer.sh
+++ build_layer.sh
@@ -7,7 +7,7 @@ printf "Select folder for Python environment:\n"
 select d in */; do test -n "$d" && break; echo ">>> Invalid Selection"; done
 
 if [ -d "$d/lib" ]; then
-
+    mkdir -p python
     cp -r "$d/lib" python/
     zip -r "deploy.zip" python
     echo "Folder created"
