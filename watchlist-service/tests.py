9f7d315fddb5502051f171b8656a84be44f6e0a0
Saaransh Sharma <saaranshsharma28@gmail.com>
2024-10-22 23:19:50 -0400

    update to deployment

--- watchlist-service/tests.py
+++ watchlist-service/tests.py
@@ -7,7 +7,6 @@ def testInvalidUser():
     mock_event = {'queryStringParameters':{
         'id': 'dddd'
     }}
-
     watchlist_response = lambda_handler(mock_event, context="")
     body = watchlist_response["body"]
      
