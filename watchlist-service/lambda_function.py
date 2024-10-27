18e2c34fa6852f763fb7855f10559173e40f595c
Saaransh Sharma <saaranshsharma28@gmail.com>
2024-10-27 18:22:53 -0400

    update python tests workflow

--- watchlist-service/lambda_function.py
+++ watchlist-service/lambda_function.py
@@ -1,10 +1,11 @@
 import json
 import os
 from supabase import create_client, Client
+from concurrent.futures import ThreadPoolExecutor
 from dotenv import load_dotenv
 import yfinance as yf
-load_dotenv()
 
+load_dotenv()
 
 url: str = os.environ.get("SUPABASE_URL")
 key: str = os.environ.get("SUPABASE_ANON_KEY")
@@ -23,13 +24,15 @@ def lambda_handler(event, context):
         'body': json.dumps({'watchlist': watchlist})
         }
 
+    query_list = generate_query_list(watchlist)
+    data_list = get_stock_info(query_list)
 
     return {
         'statusCode': 200,
         'headers': {
             'Access-Control-Allow-Origin': "*"
         },  
-        'body': json.dumps({'watchlist': watchlist})
+        'body': json.dumps({'watchlist': data_list})
     }
 
 def get_watchlist(id):
@@ -40,8 +43,7 @@ def get_watchlist(id):
     except:
         return []
 
-def generate_query_list(stock_list):
-    
+def generate_query_list(stock_list):    
     yfinancesymbols = {
     'ASX': ".AX",
     'TSX': ".TO",
@@ -61,6 +63,24 @@ def generate_query_list(stock_list):
         
     return stock_list
 
+def fetch_yfinance(item):
+    symbol = item["search_query"]
+    data = yf.Ticker(symbol).info
+
+    item["price"] = data.get('currentPrice') or data.get('regularMarketOpen') or 0
+    item["name"] = data.get('longName')
+    item["rating"] = data.get('recommendationKey', "N/A") 
+
+    if item["rating"] == 'none': item["rating"] = "N/A" 
+
+    del item["search_query"]
+    
+    return item
 
 
-# print(yf.Ticker('INTC').info)
+def get_stock_info(query_list):
+    
+    with ThreadPoolExecutor() as executor:
+        updated_list = list(executor.map(fetch_yfinance, query_list))
+
+    return updated_list
--- watchlist-service/tests.py
+++ watchlist-service/tests.py
@@ -1,9 +1,16 @@
-from lambda_function import lambda_handler, generate_query_list
+from lambda_function import lambda_handler, generate_query_list, get_stock_info
 import pytest
 import json
 from dotenv import load_dotenv
 import os
 load_dotenv()
+
+mock_list = [{'symbol': 'AMD', 'index': 'NASDAQ'}, 
+            {'symbol': 'BA', 'index': 'NYSE'}, 
+            {'symbol': 'SPY', 'index': 'ASX'},
+            {'symbol': 'BCE', 'index': 'TSX'},
+            {'symbol': 'SPY', 'index': None}   
+            ]
 
 def test_invalid_user():
 
@@ -15,7 +22,6 @@ def test_invalid_user():
     output = json.loads(body)
 
     assert not output["watchlist"]
-     
 
 def test_valid_user():
     user_id: str = os.environ.get("TEST_USER_ID")
@@ -29,17 +35,16 @@ def test_valid_user():
 
     assert output["watchlist"]
 
-def test_query_list():
-    mock_list = [{'symbol': 'AMD', 'index': 'NASDAQ'}, 
-                 {'symbol': 'BA', 'index': 'NYSE'}, 
-                 {'symbol': 'SPY', 'index': 'ASX'},
-                 {'symbol': 'BCE', 'index': 'TSX'},
-                 {'symbol': 'SPY', 'index': None}   
-                 ]
-    
+def test_query_list(): 
     query_list = generate_query_list(mock_list)
-    print(query_list)
     
     assert {'symbol': 'SPY', 'index': None, 'search_query': 'SPY'} in query_list
     assert {'symbol': 'BCE', 'index': 'TSX', 'search_query': 'BCE.TO'} in query_list
     assert {'symbol': 'SPY', 'index': 'ASX', 'search_query': 'SPY.AX'} in query_list
+
+def test_yfinance():
+    query_list = generate_query_list(mock_list)
+    finallist = get_stock_info(query_list)    
+
+    if all(all(key in result for key in ("name", "price", "rating", "symbol", "index")) for result in finallist):
+        assert True
