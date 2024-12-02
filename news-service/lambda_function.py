2b7671bf2c98e4f609b6aea4e0238d45f6b473e6
Saaransh Sharma <saaranshsharma28@gmail.com>
2024-12-01 19:46:41 -0500

    adding pinned, refactoring

--- news-service/lambda_function.py
+++ news-service/lambda_function.py
@@ -1,10 +1,11 @@
 import json
 
-# TODO: Add parameters: world, single, multiple /world , /pinned
-
 def lambda_handler(event, context):
 
-    if not (event['queryStringParameters']) or (event['queryStringParameters']['type'] is None):
+    query_string_params = event['queryStringParameters']
+    query_type = event['queryStringParameters']['type']
+
+    if not query_type:
         return {
             'statusCode': 500,
             'headers': {
@@ -14,31 +15,58 @@ def lambda_handler(event, context):
                 'message' : "No parameters provided."
             })
         }
+
+    try:
+        query_responses = {
+            'world': lambda: "Querying world news",
+            'stock': lambda:stock_news_handler(query_string_params), 
+            'pinned': lambda:pinned_news_handler(query_string_params)       
+        }
+
+        if query_type not in query_responses:
+            raise ValueError(f"Invalid query type: {query_type}")
+
+        response_val = query_responses[query_type]()
+
+        return {
+            'statusCode': 200,
+            'body': json.dumps(response_val)
+        }
+
+    except ValueError as ve:
+        return {
+            'statusCode': 400,
+            'headers': {
+                'Access-Control-Allow-Origin': "*"
+            },
+            'body': json.dumps({
+                'message': str(ve)
+            })
+        }
+
+    except KeyError as ke:
+        return {
+            'statusCode': 400,
+            'headers': {
+                'Access-Control-Allow-Origin': "*"
+            },
+            'body': json.dumps({
+                'message': f"Missing required parameter: {str(ke)}"
+            })
+        }   
+
+def stock_news_handler(query_string_obj):
+    stock_symbol = query_string_obj['symbol']
+
+    if not stock_symbol or stock_symbol is None:
+        raise KeyError("No stock provided")
     
-    query_type = event['queryStringParameters']['type']
+    return f"Querying stock news for {stock_symbol}"
 
-    response_val = ""
+def pinned_news_handler(query_string_obj):    
+    user_id = query_string_obj['id']
 
-    if query_type == 'world':
-        response_val = "Querying world news"
-    elif query_type == 'stock':
-
-        stock_symbol = event['queryStringParameters']['symbol']
-
-        if stock_symbol:
-            response_val = f"Querying stock news for {stock_symbol}"
-        else:
-            return {
-                'statusCode': 400,
-                'headers': {
-                    'Access-Control-Allow-Origin': "*"
-                },
-                'body': json.dumps({
-                    'message': "No symbol provided."
-                })
-            }
-        
-    return {
-        'statusCode': 200,
-        'body': json.dumps(response_val)
-    }
+    if not user_id or user_id is None:
+        raise KeyError("No user id provided")
+    
+    return f"Querying stock news for {user_id}"
