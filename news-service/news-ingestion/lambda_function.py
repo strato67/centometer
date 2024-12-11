4f75444ebb76d1110ec84254ed3c2c7fbfe00334
Saaransh Sharma <saaranshsharma28@gmail.com>
2024-12-10 23:50:04 -0500

    creating batch job for news archiving

--- news-service/news-ingestion/lambda_function.py
+++ news-service/news-ingestion/lambda_function.py
@@ -1,23 +1,34 @@
-import json
 import boto3
 from news_retriever import get_world_news, get_finance_news
 
 client = boto3.client('dynamodb')
 
 def lambda_handler(event, context):
-    # TODO implement
+    
+
+    world_data = get_world_news()
+    finance_data = get_finance_news()
+
+    table_updater(world_data)
+
+    table_updater(finance_data)
+
     return {
         'statusCode': 200, 
         'body': "Table updated"
     }
 
+def table_updater(news_data):
 
-def table_updater(news_type):
+    batches = [news_data[i:i + 25] for i in range(0, len(news_data), 25)]
 
-    news_data = get_finance_news() if news_type is "business" else get_world_news()
+    for batch in batches:        
+        request_item = {
+            "news-storage" : [
 
+                {"PutRequest": {"Item": item}} for item in batch
 
-    pass
-
-
-print(get_finance_news())
+            ]
+        }
+        client.batch_write_item(RequestItems=request_item)
+    print("Processing " + str(len(batches)) + " batches of " + str(len(news_data)) + " items...")
--- news-service/news-ingestion/news_retriever.py
+++ news-service/news-ingestion/news_retriever.py
@@ -5,29 +5,27 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from pygooglenews import GoogleNews
 gn = GoogleNews()
 
-def generate_response(news_data):
-  count = 0
+def generate_response(news_data, category):
+
   response = []
   for entry in news_data:
 
     response.append({
-      'id': count,
-      'title': entry["title"],
-      'source': entry["source"]['title'],
-      'date': entry["published"],
-      'url' : entry['links'][0]['href']
+      "category": {"S": category},
+      'title': {"S": entry["title"]},
+      'source': {"S": entry["source"]['title']},
+      'date': {"S": entry["published"]},
+      'url' : {"S":entry['links'][0]['href']}
     })
-
-    count += 1
 
   return response
 
 def get_world_news():
   top_news = gn.top_news()
   entries = top_news["entries"]
-  return generate_response(entries)
+  return generate_response(entries, "world")
 
 def get_finance_news():
   business = gn.topic_headlines('BUSINESS', proxies=None, scraping_bee = None)
   entries = business["entries"]
-  return generate_response(entries)
+  return generate_response(entries, "business")
