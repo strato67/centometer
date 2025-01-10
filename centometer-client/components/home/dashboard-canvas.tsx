ce4131702d4661afd36cbe25f7c27a981a9651aa
Saaransh Sharma <saaranshsharma28@gmail.com>
2025-01-09 19:04:35 -0500

    refactor dashboard component

---
+++ centometer-client/components/home/dashboard-canvas.tsx
@@ -0,0 +1,36 @@
+import PinnedCard from "@/components/home/pinned-card";
+import NewsCard from "@/components/home/news-card";
+import Heatmap from "@/components/widgets/heatmap";
+import TrendingStocksCard from "@/components/home/trending-card";
+import MarketScreenerCard from "@/components/home/market-screener-card";
+
+export default function DashboardCanvas() {
+  return (
+    <>
+      <h1 className="text-4xl font-bold">Home</h1>
+      <div className="grid justify-center grid-flow-row md:grid-cols-2 w-full gap-4 md:gap-6 mt-4">
+        <div className="md:col-span-2">
+          <PinnedCard />
+        </div>
+        <Heatmap />
+        <NewsCard
+          title="Watchlist News"
+          description="The latest news on your watchlist stocks"
+          newsType="watchlist"
+        />
+        <NewsCard
+          title="Business News"
+          description="The latest business stories"
+          newsType="business"
+        />
+        <NewsCard
+          title="World News"
+          description="Trending stories from around the world"
+          newsType="world"
+        />
+        <TrendingStocksCard />
+        <MarketScreenerCard />
+      </div>
+    </>
+  );
+}
